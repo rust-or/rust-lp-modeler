06390c9d7922efedff7c30487b1f868bc02ceb57
godsend <jcavat@gmail.com>
2016-09-28 22:34:41 +0200

    Add some unit tests for LpConstraint

--- src/tests.rs
+++ src/tests.rs
@@ -2,6 +2,7 @@ use variables::*;
 use variables::LpExpression::*;
 use problem::*;
 use std::rc::Rc;
+use operations::LpOperations;
 
 
 #[test]
@@ -26,7 +27,18 @@ fn expressions_to_string() {
     assert_eq!((a + 2).to_string(), "a + 2");
     assert_eq!((2*a + 2*b -4*c).to_string(), "2 a + 2 b - 4 c");
     assert_eq!((-2*a).to_string(), "-2 a");
-
 }
 
 
+#[test]
+fn constraints_to_string() {
+    let ref a = LpInteger::new("a");
+    let ref b = LpInteger::new("b");
+    let ref c = LpInteger::new("c");
+
+    assert_eq!((a+b).equal(10).to_string(), "a + b = 10");
+    assert_eq!((2*a + b).ge(10).to_string(), "2 a + b >= 10");
+    assert_eq!((2*a + b + 20).ge(c).to_string(), "2 a + b - c >= -20");
+    assert_eq!((-a).ge(10).to_string(), "-a >= 10");
+    assert_eq!((2*a - 20 + b).ge(-c).to_string(), "2 a + b + c >= 20");
+}
