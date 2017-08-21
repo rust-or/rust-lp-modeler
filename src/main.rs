3a3a0b48a1e93a40912821de3c42b9c1396593fd
jcavat <jcavat@gmail.com>
2017-08-21 17:49:09 +0200

    Test i64 and f64

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ extern crate lp_modeler;
 use lp_modeler::operations::LpOperations;
 use lp_modeler::problem::{LpProblem, LpObjective, LpFileFormat};
 use lp_modeler::solvers::*;
-use lp_modeler::variables::{LpInteger};
+use lp_modeler::variables::{LpInteger, LpBinary, LpExpression, LpConstraint};
+use lp_modeler::variables::LpExpression::LitVal;
 
 fn main() {
 
@@ -16,9 +17,34 @@ fn main() {
 
     problem += 10.0 * a + 20.0 * b;
 
-    problem += (500*a + 1200*b + 1500*c).le(10000);
+    problem += (a*400).le(10000);
+    problem += (400*a).le(10000);
     problem += (a + b*2 + c).le(10);
     problem += (a).le(b);
+
+    let input_coins: Vec<i64> = vec![150, 200, 300, 5000, 20323];
+    let output_coins = vec![203];
+    let fee_rate = 300;
+
+    // constants
+
+    let bytes_per_input = 92;
+    let transaction_fixed_bytes = 12;
+    let bytes_per_output = 34;
+
+
+    // problem below
+
+
+
+
+    let input_expressions: Vec<(LpBinary, LpExpression)> = input_coins.iter().enumerate().map(|(i,coin)| {
+        let constraint_name = format!("contains{}", i);
+        let ref constraint_variable = LpBinary::new(constraint_name.as_str());
+        (constraint_variable.clone() , *coin * constraint_variable)
+    }).collect();
+
+
 
     // Suboptimal instead of unfeasible
     // problem += (a+b).equal(10);
@@ -30,6 +56,7 @@ fn main() {
     let _ = problem.write_lp("toto.lp");
 
     let solver = GurobiSolver::new();
+    //let solver = CbcSolver::new();
 
     match solver.run(&problem) {
         Ok((status, res)) => {
