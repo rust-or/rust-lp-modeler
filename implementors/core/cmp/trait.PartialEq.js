98347328f7cf5d60817611d0266ecd9002e0b42e
jcavat <jcavat@gmail.com>
2017-09-05 14:51:39 +0200

    Update documentation

---
+++ implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["lp_modeler"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"lp_modeler/problem/enum.LpObjective.html\" title=\"enum lp_modeler::problem::LpObjective\">LpObjective</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"lp_modeler/solvers/enum.Status.html\" title=\"enum lp_modeler::solvers::Status\">Status</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"lp_modeler/variables/struct.LpBinary.html\" title=\"struct lp_modeler::variables::LpBinary\">LpBinary</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"lp_modeler/variables/struct.LpInteger.html\" title=\"struct lp_modeler::variables::LpInteger\">LpInteger</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"lp_modeler/variables/struct.LpContinuous.html\" title=\"struct lp_modeler::variables::LpContinuous\">LpContinuous</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"lp_modeler/variables/enum.LpExpression.html\" title=\"enum lp_modeler::variables::LpExpression\">LpExpression</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/trait.AddAssign.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["lp_modeler"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/trait.AddAssign.html\" title=\"trait core::ops::AddAssign\">AddAssign</a>&lt;<a class=\"struct\" href=\"lp_modeler/variables/struct.LpConstraint.html\" title=\"struct lp_modeler::variables::LpConstraint\">LpConstraint</a>&gt; for <a class=\"struct\" href=\"lp_modeler/problem/struct.LpProblem.html\" title=\"struct lp_modeler::problem::LpProblem\">LpProblem</a>","impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/trait.AddAssign.html\" title=\"trait core::ops::AddAssign\">AddAssign</a>&lt;T&gt; for <a class=\"struct\" href=\"lp_modeler/problem/struct.LpProblem.html\" title=\"struct lp_modeler::problem::LpProblem\">LpProblem</a> <span class=\"where fmt-newline\">where T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"enum\" href=\"lp_modeler/variables/enum.LpExpression.html\" title=\"enum lp_modeler::variables::LpExpression\">LpExpression</a>&gt;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
