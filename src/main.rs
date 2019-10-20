c506bf6966d87e710cf08bcc86640347e2dff96f
Joel Cavat <jcavat@gmail.com>
2019-10-20 23:19:14 +0200

    Use threads and seconds in this main snippet

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,19 @@ fn main() {
 
     let mut problem = LpProblem::new("Problem", LpObjective::Maximize);
 
+    // Objective Function: Maximize 10*a + 20*b
     problem += 10.0 * a + 20.0 * b;
 
-    problem += (300 * (a - b)).ge(100);
-    problem += (300 * (-a + b)).le(100);
-    problem += (a + b).le(10);
+    // Constraint: 500*a + 1200*b + 1500*c <= 10000
+    problem += (500*a + 1200*b).le(10000);
 
-    problem += sum(&vec!(a,b), |&v| v * 3.0 ).equal(10.0);
+    // Constraint: a <= b
+    problem += (a).le(b);
 
     //let _ = problem.write_lp("toto.lp");
 
 //    let solver = GurobiSolver::new();
-    let solver = CbcSolver::new();
+    let solver = CbcSolver::new().with_nb_threads(2).with_max_seconds(30);
 
     match solver.run(&problem) {
         Ok((status, res)) => {
