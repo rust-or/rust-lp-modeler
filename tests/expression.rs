eb59e89f20066a683635485e5b445516ee1a5f65
Colman Humphrey <colmanhumphrey@gmail.com>
2020-04-19 19:36:39 -0400

    adding distributive test for continuous variables too
    - note: this doesn't compile in previous version

--- tests/expression.rs
+++ tests/expression.rs
@@ -35,6 +35,15 @@ fn distributivity() {
         ((c + 10) * test2).to_lp_file_format(),
         "6 c a + 3 c a b + 60 a + 30 a b"
     );
+
+    let ref x = LpContinuous::new("x");
+    let ref y = LpContinuous::new("y");
+    let ref z = LpContinuous::new("z");
+
+    let test3 = x * (y - z);
+    assert_eq!((3 * (3 - a)).to_lp_file_format(), "-3 a + 9");
+    assert_eq!(test3.to_lp_file_format(), "x y - x z");
+    assert_eq!((4 * test3).to_lp_file_format(), "4 x y - 4 x z");
 }
 
 #[test]
