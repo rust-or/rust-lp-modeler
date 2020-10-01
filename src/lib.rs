62976cfddcc7b765cd1d8c7c7d2d2a7a3db4df1d
David Laehnemann <david.laehnemann@hhu.de>
2020-10-01 19:09:06 +0200

    remove operations.rs submodule

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,6 @@ pub mod util;
 pub mod dsl {
     pub mod variables;
     pub use self::variables::*;
-    pub mod operations;
-    pub use self::operations::*;
     pub mod problem;
     pub use self::problem::*;
 }
