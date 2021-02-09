3bd8a36ab877a9bfad78dbe07751e30109f85783
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2021-02-09 14:21:31 +0100

    Add documentation for the new macro

--- src/util.rs
+++ src/util.rs
@@ -2,7 +2,23 @@ pub fn is_zero(n: f32) -> bool {
     n.abs() < 0.00001
 }
 
-
+/// This macro allows defining constraints using 'expression1 <= expression2'
+/// instead of `expression1.le(expression2)`. 
+/// 
+/// # Example:
+///
+/// ```
+/// use lp_modeler::dsl::*;
+/// use lp_modeler::constraint;
+///
+/// let ref a = LpInteger::new("a");
+/// let ref b = LpInteger::new("b");
+///
+/// let mut problem = LpProblem::new("One Problem", LpObjective::Maximize);
+/// problem += 5*a + 3*b;
+/// problem += constraint!(a + b*2 <= 10);
+/// problem += constraint!(b >= a);
+/// ```
 #[macro_export]
 macro_rules! constraint {
     ([$($left:tt)*] <= $($right:tt)*) => {
